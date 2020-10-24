d9dee109a6e8720fff8c404841f3c92183b06172
Kim Seonghoon <ggu1012@naver.com>
2020-10-25 04:40:27 +0900

    change parser with fscanf

--- csim.c
+++ csim.c
@@ -23,28 +23,7 @@ int main(int argc, char* argv[]) {
 
    
     while (fgets(line_buf, 100, trace)!=NULL) {
-        /* line_buf has information of operation, address, size in order. */
-        /* Divide the information into three section                      */
-
-        // Pass the memory for Instruction
-        if (line_buf[0] == 'I'){
-             continue;
-        }
-        // Corner Case : if line starts with space, passes it
-        else if (line_buf[0] == ' ')
-            line_buf++;
-
-        /* Initialization of three infos. */
-        op = strtok(line_buf, " ");
-        addr_tmp = strtok(NULL, ",");
-        size_tmp = strtok(NULL, "\n");
-
-        /* Converts string to int */
-        long long int addr = strtoll(addr_tmp, NULL, 16);
-        int size = strtoll(size_tmp, NULL, 10);
-
-        printf("%s %llx,%d\n", op, addr, size);
-        //Errors at L 273 long.trace, trans.trace
+        
     }
 
     
