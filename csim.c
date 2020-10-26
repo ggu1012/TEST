d25ece053682bf4dad7f5aac26ca439887b95018
Kim Seonghoon <ggu1012@naver.com>
2020-10-27 02:40:47 +0900

    캐시 정보 빼내는거 구현해야 함.

--- csim.c
+++ csim.c
@@ -11,22 +11,60 @@ int main(int argc, char* argv[]) {
     int miss = 0;
     int evict = 0;
 
-    FILE* trace = fopen(argv[8], "r");           // open trace file
-    char* line_buf = malloc(100);  // line buffer
+    typedef struct {
+        char op;
+        unsigned long int addr;
+        int size;
+    } trace_line;  // struct for trace line.
 
-    char* op = malloc(sizeof(char)); 
-    /* address in string. 16 bytes allocated */
-    char* addr_tmp = malloc(16);  
-    /* size in string. */
-    char* size_tmp = malloc(sizeof(int)); 
-    
+    typedef struct {
+        int s;       // 2^s is the number of sets
+        int E;       // Associativity. # of lines per set
+        int b;       // 2^b is the block size
+    } cache_struct;  // Cache structure
 
-   
-    while (fgets(line_buf, 100, trace)!=NULL) {
+    typedef struct {
+        int index;
+        int tag;
+        int b_offset;  // block offset
+    } cache_info;
+
+    FILE* trace = fopen(argv[8], "r");  // open trace file
+    if (trace == NULL)                  // print error if file is invalid
+        fprintf(stderr, "Invaild Trace File.");
+
+    int result = 0;
+
+    trace_line t;
+    cache_struct cs;
+    cache_info ci;
+
+    /* Extract cache structure from arguments */
+    cs.s = atoi(argv[2]);
+    cs.E = atoi(argv[4]);
+    cs.b = atoi(argv[6]);
+
+    while (1) {
+        result = fscanf(trace, " %s %lx, %d", &t.op, &t.addr, &t.size);
+        if (result == EOF) break;  // END of trace file. break.
+
+        // abandon instruction trace
+        if (t.op == 'I') continue;
+
+        /*                  *** Address Structure ***
+        |        (64-s-b) bits       |      s bits     |   b bits   |
+        |           tag              |      index      |  b_offset  |
+        */
+
+        /* Extract Cache Information from address*/
         
+
+        
+        printf("%c %lx, %d\n", t.op, t.addr, t.size);
+        printf("Cache Info. index = %d, tag = %d, b_offset = %d\n", ci.index, ci.tag, ci.b_offset);
     }
 
-    
+    printf("Cache structure. s=%d, E=%d, b=%d\n", cs.s, cs.E, cs.b);
     printSummary(hit, miss, evict);
 
     return 0;
