a484aeec6eb7cf907014c388d6033dfc9a2dd408
Kim Seonghoon <ggu1012@naver.com>
2020-10-27 18:15:30 +0900

    캐시 라인 구현

--- csim.c
+++ csim.c
@@ -6,43 +6,51 @@
 
 #include "cachelab.h"
 
+typedef struct {
+    char op;
+    unsigned long int addr;
+    int size;
+} trace_line;  // struct for trace line.
+
+typedef struct {
+    int s;       // 2^s is the number of sets
+    int E;       // Associativity. # of lines per set
+    int b;       // 2^b is the block size
+} cache_struct;  // Cache structure
+
 int main(int argc, char* argv[]) {
     int hit = 0;
     int miss = 0;
     int evict = 0;
 
-    typedef struct {
-        char op;
-        unsigned long int addr;
-        int size;
-    } trace_line;  // struct for trace line.
-
-    typedef struct {
-        int s;       // 2^s is the number of sets
-        int E;       // Associativity. # of lines per set
-        int b;       // 2^b is the block size
-    } cache_struct;  // Cache structure
-
-    typedef struct {
-        int index;
-        int tag;
-        int b_offset;  // block offset
-    } cache_info;
+    /* Address Structure variable */
+    int index;
+    int tag;
+    int block_offset;
 
     FILE* trace = fopen(argv[8], "r");  // open trace file
     if (trace == NULL)                  // print error if file is invalid
-        fprintf(stderr, "Invaild Trace File.");
+        fprintf(stderr, "Invaild Trace File.\n");
 
     int result = 0;
 
     trace_line t;
     cache_struct cs;
-    cache_info ci;
 
     /* Extract cache structure from arguments */
     cs.s = atoi(argv[2]);
     cs.E = atoi(argv[4]);
     cs.b = atoi(argv[6]);
+
+    /* Cache Line implementation */
+    typedef struct {
+        int tag[cs.E];     // Stored tags
+        int RUinfo[cs.E];  // Which tag is Recently Used? !! 0: Recently Used
+    } cache_line;
+
+    /* Cache Initialization w/ Memory allocation */
+    cache_line* cache;
+    cache = malloc(sizeof(cache_line) * cs.s);  // initializes the total cache = cache_line * set
 
     while (1) {
         result = fscanf(trace, " %s %lx, %d", &t.op, &t.addr, &t.size);
@@ -51,21 +59,27 @@ int main(int argc, char* argv[]) {
         // abandon instruction trace
         if (t.op == 'I') continue;
 
-        /*                  *** Address Structure ***
+        /*
+                            *** Address Implementation ***
         |        (64-s-b) bits       |      s bits     |   b bits   |
         |           tag              |      index      |  b_offset  |
+
         */
 
         /* Extract Cache Information from address*/
-        
+        index = t.addr << (64 - cs.s - cs.b) >> (64 - cs.s);
+        tag = t.addr >> (cs.s + cs.b);
+        block_offset = t.addr << (64 - cs.b) >> (64 - cs.b);
 
-        
         printf("%c %lx, %d\n", t.op, t.addr, t.size);
-        printf("Cache Info. index = %d, tag = %d, b_offset = %d\n", ci.index, ci.tag, ci.b_offset);
+        printf("Cache Info. tag = %d, index = %d, block_offset = %d\n", tag,
+               index, block_offset);
     }
 
     printf("Cache structure. s=%d, E=%d, b=%d\n", cs.s, cs.E, cs.b);
     printSummary(hit, miss, evict);
 
+    // Free cache_line
+    free(cache);
     return 0;
 }
