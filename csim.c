93161a0e9f5adfd831d732a97d158bd2297b1cfe
Kim Seonghoon <ggu1012@naver.com>
2020-10-30 18:35:26 +0900

    연결 리스트 완성. free 수정 필요

--- csim.c
+++ csim.c
@@ -6,56 +6,88 @@
 
 #include "cachelab.h"
 
-typedef struct trace_line{
-    char op;                 // L, S or M
+typedef struct trace_line {
     unsigned long int addr;  // Address. uint64
     int size;
+    char op;   // L, S or M
 } trace_line;  // struct for trace line.
 
-int main(int argc, char* argv[]) {
+typedef struct tag_info {
+    long int tag;  // tag bit
+    struct tag_info* prev;
+    struct tag_info* next;  // Linked List
+} tag_info;
 
+tag_info* InsertNode(long int x, struct tag_info* head);
+void DeleteNode(struct tag_info* tag);
+void freeList(struct tag_info* head);
+
+int main(int argc, char* argv[]) {
     int hit = 0;
     int miss = 0;
     int evict = 0;
 
-    int index, tag;
-    int s, E, b;
-    
+    int index;     // index
+    long int tag;  // tag
+
+    int s;  // set index bits
+    int E;  // Associativity. # of lines per set
+    int b;  // block offset
+
     /* Extract cache structure from arguments */
     s = atoi(argv[2]);
     E = atoi(argv[4]);
     b = atoi(argv[6]);
 
-    int total_set = s << 1; // total_set # = 2^s
+    int total_set = 1 << s;  // total_set # = 2^s
 
     /* Cache Line implementation */
-    typedef struct cache_line{
-        long int tag[E];     // Stored tags
-        int LRU[E]; // if it is LRU tag, LRU[i] = 1;
-        int valid_bit;     // Valid bit.
+    typedef struct cache_line {
+        tag_info* base;
+        int valid_bit;  // Valid bit.
     } cache_line;
 
-    /* Cache Initialization w/ Memory allocation */
-    cache_line* cache = malloc(sizeof(cache_line) * total_set); 
+    /* Cache Line Memory allocation */
+    struct cache_line* cache = malloc(sizeof(struct cache_line) * total_set);
     // initializes the total cache = cache_line * set
-    
-    /* Every Line Buffer*/
-    trace_line* t = malloc(sizeof(trace_line));
 
+    struct tag_info* tmp_base; // buffer for Linked List
+    struct tag_info* new;
+
+    /* Cache Initialization */
+
+    for (int i = 0; i < total_set; ++i) {
+        cache[i].valid_bit = 0;
+        /* Memory allocation. base of the tag list */
+        cache[i].base = malloc(sizeof(tag_info));
+        /* tmp for LinkedList*/
+        tmp_base = cache[i].base;
+        tmp_base->prev = NULL;  // head -> prev = NULL;
+
+        /* Linked List. Malloc next node */
+        for (int j = 0; j < E - 1; ++j) {
+            tmp_base->next = malloc(sizeof(tag_info));
+            tmp_base->next->prev = tmp_base;
+            // if tag field is empty, tag = -1
+            tmp_base->tag = -1;
+            tmp_base = tmp_base->next;
+        }
+        // Now, tmp_base = tail
+        tmp_base->tag = -1;
+        tmp_base->next = NULL;  // tail -> next = NULL
+    }
+
+    int result = 0;  // Checks if file read reached end or not.
 
     FILE* trace = fopen(argv[8], "r");  // open trace file
     if (trace == NULL)                  // print error if file is invalid
         fprintf(stderr, "Invaild Trace File.\n");
 
-
-    for (int i = 0; i < total_set; ++i) {
-        for (int j = 0; j < E; ++j)
-            cache[i].tag[j] = -1;  // tag field is empty if tag == -1
-        cache[i].valid_bit = 0;
-    }
-    int result = 0;  // Checks if file read reached end or not.
+    /* Every Line Buffer*/
+    struct trace_line* t = malloc(sizeof(struct trace_line));
 
     while (1) {
+        // Read File Line by Line
         result = fscanf(trace, " %s %lx, %d", &t->op, &t->addr, &t->size);
 
         // END of trace file. break.
@@ -84,89 +116,129 @@ int main(int argc, char* argv[]) {
         int tmp_miss = 0;
         int tmp_evict = 0;
 
-        /* First, checkout the memory access type */
-        switch (t->op) {
-            /* Load. */
-            case 'L':
-                /* Cache Walk. */
-                /* Index is filled with some data */
-                if (cache[index].valid_bit == 1) {
-                    /* Now Checks the tag exists in the cache line */
-                    for (int tmp_line = 0; tmp_line < E; ++tmp_line) {
-                        /* Hit ! */
-                        if (cache[index].tag[tmp_line] == tag) {
-                            tmp_hit = 1;
-                            /* if Hit is LRU component, change LRU bit location*/
-                            for(int i = 0; i < E; ++i)
-                                if(cache[index.LRU[i]])
-                                cache[index].LRU[i] = 0;
-                            
-                            break;
-                        }
-                    }
-                    if (tmp_hit != 1) {
-                        /* Miss */
-                        tmp_miss = 1;
-                        tmp_evict = 1;  // First, set tmp_evict = 1
-                        /* Checks where cache.RUinfo == E - 1,
-                         * which is Least Recently Used */
-                        for (int tmp_line = 0; tmp_line < E; ++tmp_line) {
-                            if (cache[index].RUinfo[tmp_line] == E - 1) {
-                                /* If tag field is empty, Eviction does not
-                                 * happened */
-                                if (cache[index].tag[tmp_line] == -1)
-                                    tmp_evict = 0;
-                                /* tag update */
-                                cache[index].tag[tmp_line] = tag;
-                                /* Update Recently Used data */
-                                for (int i = 0; i < E; ++i)
-                                    ++cache[index].RUinfo[i];
-                                cache[index].RUinfo[tmp_line] = 0;
-                                break;
-                            }
-                        }
-                    }
-                    /* Miss, Fill the empty index */
-                } else {
-                    tmp_miss = 1;
-                    cache[index].tag[0] = tag;
-                    cache[index].valid_bit = 1;
-                    /* Set RUinfo array as {0, 1, ..., E-1} */
-                    for (int i = 0; i < E; ++i) cache[index].RUinfo[i] = i;
+        /* Cache Walk. */
+        /* Index is filled with some data */
+        if (cache[index].valid_bit == 1) {
+            tmp_base = cache[index].base;
+            /* Now Checks the tag exists in the cache line */
+            while (tmp_base != NULL) {
+                if (tmp_base->tag == tag) {
+                    tmp_hit = 1;
+                    // Realign the list for LRU algorithm
+                    // duplicate node and set as head
+                    cache[index].base = InsertNode(tag, cache[index].base);
+                    // Delete the current hit node
+                    DeleteNode(tmp_base);
+                    break;
                 }
-            
-            /* Store. */
-            case 'S':;
-
-            /* Modify. Load and Store in order */
-            case 'M':;
-            default:;
+                tmp_base = tmp_base->next;
+            }
+            if (tmp_hit != 1) {
+                /* Miss */
+                tmp_miss = 1;
+                tmp_base = cache[index].base;
+                /* Find LRU node (tail) */
+                while (tmp_base->next != NULL) {
+                    tmp_base = tmp_base->next;
+                }
+                /* if tail node is not empty, eviction = 1 */
+                if (tmp_base->tag != -1) tmp_evict = 1;
+                /* Now, tmp_base : tail */
+                cache[index].base = InsertNode(tag, cache[index].base);
+                DeleteNode(tmp_base);
+            }
+            /* Miss, Fill the empty index */
+        } else {
+            tmp_base = cache[index].base;
+            tmp_miss = 1;
+            cache[index].valid_bit = 1;
+            cache[index].base = InsertNode(tag, cache[index].base);
+            // Delete next node
+            DeleteNode(cache[index].base->next);
+            cache[index].base->next = NULL;
         }
 
+        // If memory access type is 'M',
+        // store(after load) is always hit
+        if (t->op == 'M') ++tmp_hit;
+
+        printf("%c %lx,%d ", t->op, t->addr, t->size);
+        if (tmp_miss == 1) printf("miss ");
+        if (tmp_evict == 1) printf("eviction ");
         if (tmp_hit == 1)
-            printf("%c %lx,%d hit\n", t->op, t->addr, t->size);
-        else if (tmp_miss == 1) {
-            if (tmp_evict == 1)
-                printf("%c %lx,%d miss eviction\n", t->op, t->addr, t->size);
-            else
-                printf("%c %lx,%d miss\n", t->op, t->addr, t->size);
-        }
+            printf("hit ");
+        else if (tmp_hit == 2)
+            printf("hit hit ");
+
+        printf("\n");
 
         hit += tmp_hit;
         miss += tmp_miss;
         evict += tmp_evict;
     }
 
-    printf("Cache structure. s=%d, E=%d, b=%d\n", s, E, b);
-    printSummary(hit, miss, evict);
+    for(int i=0; i<total_set; ++i)
+        freeList(cache[i].base);
 
-    // Free cache_line
     free(cache);
     cache = NULL;
 
     free(t);
     t = NULL;
 
+    printSummary(hit, miss, evict);
+
+    // Free cache_line
+    /*
+    free(tmp_base);
+    tmp_base = NULL;
+    */
+
     fclose(trace);
     return 0;
+}
+
+// Insert tag node at front
+// return new head
+tag_info* InsertNode(long int x, struct tag_info* head) {
+    // Generate New Node with fresh data
+    struct tag_info* new = malloc(sizeof(struct tag_info));
+    new->tag = x;
+    // Rebase
+    new->next = head;
+    head->prev = new;
+    new->prev = NULL;
+    // Now, NULL -> new -> head -> ...
+    return new;
+}
+
+// Delete node and realign
+void DeleteNode(struct tag_info* tag) {
+    /* First, realign the data */
+    // if input is tail
+    if (tag->next == NULL) {
+        // prev -> tag -> NULL
+        // prev     ->    NULL
+        tag->prev->next = NULL;
+        // if input is in the middle
+    } else {
+        // prev -> <- tag -> <- next
+        // prev     -> <-    next
+        tag->prev->next = tag->next;
+        tag->next->prev = tag->prev;    }
+
+    /* Then, delete the node */
+    tag->next = NULL;
+    tag->prev = NULL;
+    free(tag);
+}
+
+// Free List
+void freeList(struct tag_info* head) {
+    struct tag_info* tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp); tmp=NULL;
+    }
 }
