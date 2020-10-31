15cb867c62d0d9b7d5e9ffaf0d12571fbabb74d0
Kim Seonghoon <ggu1012@naver.com>
2020-11-01 03:43:03 +0900

    이거 내도 되냐 64x64

--- trans.c
+++ trans.c
@@ -121,7 +121,47 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
         }
     }
 
-    else if (M == 67 && N == 61) {
+    else if (M == 61 && N == 67) {
+        // stride = 16
+        for (i = 0; i < N; i += 16) {
+            // horizontal block movement. left
+            for (j = 0; j < M; j += 8) {
+                // inside one block
+                // Check smaller than N for N%8!=0 boundary
+                for (k = i; k < (i + 16) && (k < N); ++k) {
+                    for (x = j; (x < j + 16) && (j < M); ++x) {
+                        if (x != k) {
+                            B[x][k] = A[k][x];
+                        } else {
+                            a = x;  // save where diagonal
+                            b = A[x][x];
+                        }
+                        B[a][a] = b;
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*
+ * You can define additional transpose functions below. We've defined
+ * a simple one below to help you get started.
+ */
+
+/*
+ * trans - A simple baseline transpose function, not optimized for the
+ * cache.
+ */
+
+char tmp_desc[] = "64x64";
+void __64x64(int M, int N, int A[N][M], int B[M][N]) {
+    /* For submission */
+
+    int i, j, k;
+    int a, b, c, d, e, x, y, z;
+
+    if (M == 32 && N == 32) {
         // stride of main block = 8
         // vertical block movement. down
         for (i = 0; i < N; i += 8) {
@@ -153,17 +193,116 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
             }
         }
     }
+
+    else if (M == 64 && N == 64) {
+        // 8x8 main block
+        // 4x8 sub block
+        // 4x4 4x4 sub block
+
+        // Main block 8x8
+        // vertical movement
+        for (i = 0; i < N; i += 8) {
+            // horizontal movement
+            for (j = 0; j < M; j += 8) {
+                // vertical movement
+                // Sub block 4x8
+                // vertical movement
+                for (k = i; k < i + 4; ++k) {
+                    // saving values in horizontal movement
+                    a = A[k][j];
+                    b = A[k][j + 1];
+                    c = A[k][j + 2];
+                    d = A[k][j + 3];
+                    e = A[k][j + 4];
+                    x = A[k][j + 5];
+                    y = A[k][j + 6];
+                    z = A[k][j + 7];
+
+                    // Save half part of B
+                    // to reduce eviction
+                    // saving in vertical movement
+                    B[j][k] = a;
+                    B[j + 1][k] = b;
+                    B[j + 2][k] = c;
+                    B[j + 3][k] = d;
+
+                    // Other half is temporarily saved
+                    // at nearby B block, where "hit" is induced
+                    B[j][k + 4] = e;
+                    B[j + 1][k + 4] = x;
+                    B[j + 2][k + 4] = y;
+                    B[j + 3][k + 4] = z;
+                }
+
+                // Sub block 4x4 (i+4, j) ~ (i+7, j+3)
+                for (k = j; k < j + 4; ++k) {
+                    a = A[i + 4][k];
+                    b = A[i + 5][k];
+                    c = A[i + 6][k];
+                    d = A[i + 7][k];
+
+                    // now, load temporarily saved block and get out
+                    // to replace with original block at that space
+                    e = B[k][i + 4];
+                    x = B[k][i + 5];
+                    y = B[k][i + 6];
+                    z = B[k][i + 7];
+
+                    // Then replace the block with new value
+                    // a, b, c, d to get advantage of remaining "hit" condition
+                    // induced by e, x, y ,z load
+                    B[k][i + 4] = a;
+                    B[k][i + 5] = b;
+                    B[k][i + 6] = c;
+                    B[k][i + 7] = d;
+
+                    // Align B array with the data
+                    // from 8x8 block
+                    // B[j+4][i~i+4] ~ B[j+7][i~i+4]
+                    B[k+4][i] = e;
+                    B[k+4][i+1] = x;
+                    B[k+4][i+2] = y;
+                    B[k+4][i+3] = z;
+                }
+
+                // Handle another remaining 4x4 sub block
+                for (k = i + 4; k < i + 8; ++k) {
+                    a = A[k][j + 4];
+                    b = A[k][j + 5];
+                    c = A[k][j + 6];
+                    d = A[k][j + 7];
+
+                    B[j + 4][k] = a;
+                    B[j + 5][k] = b;
+                    B[j + 6][k] = c;
+                    B[j + 7][k] = d;
+                }
+            }
+        }
+    }
+
+    else if (M == 61 && N == 67) {
+        // stride = 16
+        for (i = 0; i < N; i += 16) {
+            // horizontal block movement. left
+            for (j = 0; j < M; j += 16) {
+                // inside one block
+                // Check smaller than N for N%8!=0 boundary
+                for (k = i; k < (i + 16) && (k < N); ++k) {
+                    for (x = j; (x < j + 16) && (j < M); ++x) {
+                        if (x != k) {
+                            B[x][k] = A[k][x];
+                        } else {
+                            a = x;  // save where diagonal
+                            b = A[x][x];
+                        }
+                        B[a][a] = b;
+                    }
+                }
+            }
+        }
+    }
 }
-
-/*
- * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started.
- */
-
-/*
- * trans - A simple baseline transpose function, not optimized for the
- * cache.
- */
 
 char first_desc[] = "First Prototype. block-split";
 void zig_zag(int M, int N, int A[N][M], int B[M][N]) {
@@ -259,8 +398,8 @@ void registerFunctions() {
     registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
+    registerTransFunction(__64x64, tmp_desc);
     registerTransFunction(zig_zag, first_desc);
-    registerTransFunction(nn, tmp);
     registerTransFunction(trans, trans_desc);
 }
 
