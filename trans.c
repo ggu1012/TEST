c3fddaaf4b6a494260602df47ae265779e45b009
Kim Seonghoon <ggu1012@naver.com>
2020-11-01 00:00:52 +0900

    64x64 1651 miss

--- trans.c
+++ trans.c
@@ -24,31 +24,132 @@ char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
     /* For submission */
 
-    int st_row = 8;
-    int st_col = 8;  // stride for row/column
-    int tmp;         // save element
-    int zigzag = 0;
+    int i, j, k;
+    int a, b, c, d, e, x, y, z, w;
 
-    // horizontal block movement
-    for (int i = 0; i < M; i += st_row) {        
-        //vertical block movement
-        for (int j = 0; j < N; j += st_col) {
-            /* inside one block */
-            // vertical movement
-            for (int k = i; k < i + st_col; k++) {
-                // horizontal movement
-                for (int l = j; l < j + st_row; l++) {
-                    // ->
-                    if (zigzag == 0) {
-                        tmp = A[k][l];
-                        B[l][k] = tmp;
-                    } else {
-                        // <-
-                        tmp = A[k][2*j + st_row - l -1];
-                        B[2*j + st_row - l -1][k] = tmp;
-                    }                    
+    if (M == 32 && N == 32) {
+        // stride of main block = 8
+        // vertical block movement. down
+        for (i = 0; i < N; i += 8) {
+            // horizontal block movement. left
+            for (j = M - 8; j >= 0; j -= 8) {
+                /* inside one block */
+                // vertical stride of sub-block = 4
+                // vertical movement
+                for (k = i; k < i + 8; k++) {
+                    // First, load A elements
+                    a = A[k][j];
+                    b = A[k][j + 1];
+                    c = A[k][j + 2];
+                    d = A[k][j + 3];
+                    e = A[k][j + 4];
+                    x = A[k][j + 5];
+                    y = A[k][j + 6];
+                    z = A[k][j + 7];
+                    // Then, store B elements
+                    B[j][k] = a;
+                    B[j + 1][k] = b;
+                    B[j + 2][k] = c;
+                    B[j + 3][k] = d;
+                    B[j + 4][k] = e;
+                    B[j + 5][k] = x;
+                    B[j + 6][k] = y;
+                    B[j + 7][k] = z;
                 }
-                zigzag = !zigzag;
+            }
+        }
+    }
+
+    else if (M == 64 && N == 64) {
+        /* Main block */
+        // stride of main block = 8
+        // horizontal block movement.
+        for (j = 0; j < M; j += 8) {
+            // vertical block movement. down
+            for (i = 0; i < N; i += 8) {
+                /* sub-block */
+                // if the main block is in diagonal position,
+                // index is all the same.
+                if (i == j) {
+                    // stride of sub-block = 4
+                    // horizontal movement
+                    for (a = j; a < j + 8; a += 4) {
+                        // vertical movement
+                        for (b = i; b < i + 8; b += 4) {
+                            /* inside sub block */
+                            for (c = b; c < b + 4; c++) {
+                                // First, load A elements
+                                x = A[c][a];
+                                y = A[c][a + 1];
+                                z = A[c][a + 2];
+                                w = A[c][a + 3];
+
+                                // Then, store B elements
+                                B[a][c] = x;
+                                B[a + 1][c] = y;
+                                B[a + 2][c] = z;
+                                B[a + 3][c] = w;
+                            }
+                        }
+                    }
+
+                } else {
+                    /* sub-block */
+                    // vertical stride of sub-block = 4
+                    // vertical movement
+                    for (a = j; a < j + 8; a += 4) {
+                        // horizontal movement
+                        for (b = i; b < i + 8; b += 4) {
+                            /* inside sub block */
+                            for (c = b; c < b + 4; c++) {
+                                // First, load A elements
+                                x = A[c][a];
+                                y = A[c][a + 1];
+                                z = A[c][a + 2];
+                                w = A[c][a + 3];
+
+                                // Then, store B elements
+                                B[a][c] = x;
+                                B[a + 1][c] = y;
+                                B[a + 2][c] = z;
+                                B[a + 3][c] = w;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    else if (M == 67 && N == 61) {
+        // stride of main block = 8
+        // vertical block movement. down
+        for (i = 0; i < N; i += 8) {
+            // horizontal block movement. left
+            for (j = M - 8; j >= 0; j -= 8) {
+                /* inside one block */
+                // vertical stride of sub-block = 4
+                // vertical movement
+                for (k = i; k < i + 8; k++) {
+                    // First, load A elements
+                    a = A[k][j];
+                    b = A[k][j + 1];
+                    c = A[k][j + 2];
+                    d = A[k][j + 3];
+                    e = A[k][j + 4];
+                    x = A[k][j + 5];
+                    y = A[k][j + 6];
+                    z = A[k][j + 7];
+                    // Then, store B elements
+                    B[j][k] = a;
+                    B[j + 1][k] = b;
+                    B[j + 2][k] = c;
+                    B[j + 3][k] = d;
+                    B[j + 4][k] = e;
+                    B[j + 5][k] = x;
+                    B[j + 6][k] = y;
+                    B[j + 7][k] = z;
+                }
             }
         }
     }
@@ -60,7 +161,8 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
  */
 
 /*
- * trans - A simple baseline transpose function, not optimized for the cache.
+ * trans - A simple baseline transpose function, not optimized for the
+ * cache.
  */
 
 char first_desc[] = "First Prototype. block-split";
@@ -88,38 +190,48 @@ void zig_zag(int M, int N, int A[N][M], int B[M][N]) {
 }
 
 char tmp[] = "temporary";
+void nn(int M, int N, int A[N][M], int B[M][N]) { /* For submission */
 
-void nn(int M, int N, int A[N][M], int B[M][N]) {
-    /* For submission */
+    int i, j, k;
+    int a, b, c, d, e, f, g, h;  // save element
 
-    int st_row = 8;
-    int st_col = 8;  // stride for row/column
-    int tmp;         // save element
-    int zigzag;
-
-    // Every explanation is based on array A.
-    for (int i = N - 1; i >= 0; i -= st_col) {
-        // horizontal block movement
-        for (int j = M - 1; j >= 0; j -= st_row) {
-            /* inside one block */
-            // vertical movement
-            for (int k = i; k > i - st_col; --k) {
-                zigzag = 0;
-                // horizontal movement
-                for (int l = j; l > j - st_row; --l) {
-                    // <-
-                    if (zigzag == 0) {
-                        tmp = A[k][l];
-                        B[l][k] = tmp;
-                    } else {
-                        // ->
-                        tmp = A[k][2 * j - st_row - l];
-                        B[2 * j - st_row - l][k] = tmp;
-                    }                   
+    if (M == 32 && N == 32) {
+        // stride of main block = 8
+        // vertical block movement. down
+        for (i = 0; i < N; i += 8) {
+            // horizontal block movement. left
+            for (j = M - 8; j >= 0; j -= 8) {
+                /* inside one block */
+                // vertical stride of sub-block = 4
+                // vertical movement
+                for (k = i; k < i + 8; k++) {
+                    // First, load A elements
+                    a = A[k][j];
+                    b = A[k][j + 1];
+                    c = A[k][j + 2];
+                    d = A[k][j + 3];
+                    e = A[k][j + 4];
+                    f = A[k][j + 5];
+                    g = A[k][j + 6];
+                    h = A[k][j + 7];
+                    // Then, store B elements
+                    B[j][k] = a;
+                    B[j + 1][k] = b;
+                    B[j + 2][k] = c;
+                    B[j + 3][k] = d;
+                    B[j + 4][k] = e;
+                    B[j + 5][k] = f;
+                    B[j + 6][k] = g;
+                    B[j + 7][k] = h;
                 }
-                 zigzag = !zigzag;
             }
         }
+    }
+
+    else if (M == 64 && N == 64) {
+    }
+
+    else if (M == 67 && N == 61) {
     }
 }
 
