dda4b14f33c7acf69d7b4580bd49752e7dba796b
Kim Seonghoon <ggu1012@naver.com>
2020-11-01 05:58:20 +0900

    진짜 끝

--- trans.c
+++ trans.c
@@ -26,8 +26,11 @@ char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
     /* For submission */
 
+    // iteration variable
     int i, j, k;
-    int a, b, c, d, e, x, y, z;
+
+    // Save values in register
+    int ra, rb, rc, rd, re, rx, ry, rz;
 
     if (M == 32 && N == 32) {
         // stride of main block = 8
@@ -36,27 +39,27 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
             // horizontal block movement. left
             for (j = M - 8; j >= 0; j -= 8) {
                 /* inside one block */
-                // vertical stride of sub-block = 4
+                // vertical stride of sub-block = 8
                 // vertical movement
                 for (k = i; k < i + 8; k++) {
                     // First, load A elements
-                    a = A[k][j];
-                    b = A[k][j + 1];
-                    c = A[k][j + 2];
-                    d = A[k][j + 3];
-                    e = A[k][j + 4];
-                    x = A[k][j + 5];
-                    y = A[k][j + 6];
-                    z = A[k][j + 7];
+                    ra = A[k][j];
+                    rb = A[k][j + 1];
+                    rc = A[k][j + 2];
+                    rd = A[k][j + 3];
+                    re = A[k][j + 4];
+                    rx = A[k][j + 5];
+                    ry = A[k][j + 6];
+                    rz = A[k][j + 7];
                     // Then, store B elements
-                    B[j][k] = a;
-                    B[j + 1][k] = b;
-                    B[j + 2][k] = c;
-                    B[j + 3][k] = d;
-                    B[j + 4][k] = e;
-                    B[j + 5][k] = x;
-                    B[j + 6][k] = y;
-                    B[j + 7][k] = z;
+                    B[j][k] = ra;
+                    B[j + 1][k] = rb;
+                    B[j + 2][k] = rc;
+                    B[j + 3][k] = rd;
+                    B[j + 4][k] = re;
+                    B[j + 5][k] = rx;
+                    B[j + 6][k] = ry;
+                    B[j + 7][k] = rz;
                 }
             }
         }
@@ -76,73 +79,73 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
                 // vertical movement
                 for (k = i; k < i + 4; ++k) {
                     // saving values in horizontal movement
-                    a = A[k][j];
-                    b = A[k][j + 1];
-                    c = A[k][j + 2];
-                    d = A[k][j + 3];
-                    e = A[k][j + 4];
-                    x = A[k][j + 5];
-                    y = A[k][j + 6];
-                    z = A[k][j + 7];
+                    ra = A[k][j];
+                    rb = A[k][j + 1];
+                    rc = A[k][j + 2];
+                    rd = A[k][j + 3];
+                    re = A[k][j + 4];
+                    rx = A[k][j + 5];
+                    ry = A[k][j + 6];
+                    rz = A[k][j + 7];
 
                     // Save half part of B
                     // to reduce eviction
                     // saving in vertical movement
-                    B[j][k] = a;
-                    B[j + 1][k] = b;
-                    B[j + 2][k] = c;
-                    B[j + 3][k] = d;
+                    B[j][k] = ra;
+                    B[j + 1][k] = rb;
+                    B[j + 2][k] = rc;
+                    B[j + 3][k] = rd;
 
                     // Other half is temporarily saved
                     // at nearby B block, where "hit" is induced
-                    B[j][k + 4] = e;
-                    B[j + 1][k + 4] = x;
-                    B[j + 2][k + 4] = y;
-                    B[j + 3][k + 4] = z;
+                    B[j][k + 4] = re;
+                    B[j + 1][k + 4] = rx;
+                    B[j + 2][k + 4] = ry;
+                    B[j + 3][k + 4] = rz;
                 }
 
                 // Sub block 4x4 (i+4, j) ~ (i+7, j+3)
                 for (k = j; k < j + 4; ++k) {
-                    a = A[i + 4][k];
-                    b = A[i + 5][k];
-                    c = A[i + 6][k];
-                    d = A[i + 7][k];
+                    ra = A[i + 4][k];
+                    rb = A[i + 5][k];
+                    rc = A[i + 6][k];
+                    rd = A[i + 7][k];
 
                     // now, load temporarily saved block and get out
                     // to replace with original block at that space
-                    e = B[k][i + 4];
-                    x = B[k][i + 5];
-                    y = B[k][i + 6];
-                    z = B[k][i + 7];
+                    re = B[k][i + 4];
+                    rx = B[k][i + 5];
+                    ry = B[k][i + 6];
+                    rz = B[k][i + 7];
 
                     // Then replace the block with new value
                     // a, b, c, d to get advantage of remaining "hit" condition
                     // induced by e, x, y ,z load
-                    B[k][i + 4] = a;
-                    B[k][i + 5] = b;
-                    B[k][i + 6] = c;
-                    B[k][i + 7] = d;
+                    B[k][i + 4] = ra;
+                    B[k][i + 5] = rb;
+                    B[k][i + 6] = rc;
+                    B[k][i + 7] = rd;
 
                     // Align B array with the data
                     // from 8x8 block
                     // B[j+4][i~i+4] ~ B[j+7][i~i+4]
-                    B[k + 4][i] = e;
-                    B[k + 4][i + 1] = x;
-                    B[k + 4][i + 2] = y;
-                    B[k + 4][i + 3] = z;
+                    B[k + 4][i] = re;
+                    B[k + 4][i + 1] = rx;
+                    B[k + 4][i + 2] = ry;
+                    B[k + 4][i + 3] = rz;
                 }
 
                 // Handle another remaining 4x4 sub block
                 for (k = i + 4; k < i + 8; ++k) {
-                    a = A[k][j + 4];
-                    b = A[k][j + 5];
-                    c = A[k][j + 6];
-                    d = A[k][j + 7];
+                    ra = A[k][j + 4];
+                    rb = A[k][j + 5];
+                    rc = A[k][j + 6];
+                    rd = A[k][j + 7];
 
-                    B[j + 4][k] = a;
-                    B[j + 5][k] = b;
-                    B[j + 6][k] = c;
-                    B[j + 7][k] = d;
+                    B[j + 4][k] = ra;
+                    B[j + 5][k] = rb;
+                    B[j + 6][k] = rc;
+                    B[j + 7][k] = rd;
                 }
             }
         }
@@ -156,28 +159,28 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
             for (j = 0; j < M; j += 8) {
                 // block inside
                 for (k = i; k < i + 8 && k < N; ++k) {
-                    a = A[k][j];
-                    b = A[k][j + 1];
-                    c = A[k][j + 2];
-                    d = A[k][j + 3];
-                    e = A[k][j + 4];
+                    ra = A[k][j];
+                    rb = A[k][j + 1];
+                    rc = A[k][j + 2];
+                    rd = A[k][j + 3];
+                    re = A[k][j + 4];
                     // if block reaches the end, skip this part
                     if (j + 4 != M - 1) {
-                        x = A[k][j + 5];
-                        y = A[k][j + 6];
-                        z = A[k][j + 7];
+                        rx = A[k][j + 5];
+                        ry = A[k][j + 6];
+                        rz = A[k][j + 7];
                     }
                     // Then, store B elements
-                    B[j][k] = a;
-                    B[j + 1][k] = b;
-                    B[j + 2][k] = c;
-                    B[j + 3][k] = d;
-                    B[j + 4][k] = e;
+                    B[j][k] = ra;
+                    B[j + 1][k] = rb;
+                    B[j + 2][k] = rc;
+                    B[j + 3][k] = rd;
+                    B[j + 4][k] = re;
                     // if block reaches the end, skip this part
                     if (j + 4 != M - 1) {
-                        B[j + 5][k] = x;
-                        B[j + 6][k] = y;
-                        B[j + 7][k] = z;
+                        B[j + 5][k] = rx;
+                        B[j + 6][k] = ry;
+                        B[j + 7][k] = rz;
                     }
                 }
             }
